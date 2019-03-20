b92e6eb384a429bcba85121983559f532c1e4f5e
Augusto <you@example.com>
2019-03-20 12:33:02 -0300

    Punto 1 completo

---
+++ tp1_1.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+	int num = 5;
+	int *p_num;
+
+	p_num = &num;
+
+
+	printf("el contenido del puntero: %d\n", *p_num);
+	printf("direccion de memoria almacenada por el puntero: %d\n", p_num);
+	printf("la direccion de memoria de la variable: %d\n", &num);
+	printf("direccion de memoria del puntero: %d\n", &p_num);
+	printf("el tamani0o de memoria usado por la variable: %d\n", sizeof(num));
+
+/*
+	Se obtiene lo mismo debido a que el puntero almacena la direccion de memoria de la variable 
+	En el punto 4 se obtiene la direccion de memoria en la que se guarda el puntero; por eso es diferente		
+*/
+
+
+}
